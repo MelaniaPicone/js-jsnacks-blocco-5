c1a4fad6ae7e0a191ecade6cc67f983f30c5e7ae
MelaniaPicone <melania_picone@yahoo.it>
2025-07-18 09:17:26 +0200

    [add] 5 snack

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,20 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenNums = nums.filter ((nums) => {
+
+if(nums * 2 == 0) {
+  return true;
+}
+
+else {
+  return false;
+}
+
+return nums *2 == 0;
+
+}
+)
+
+console.log(evenNums);
