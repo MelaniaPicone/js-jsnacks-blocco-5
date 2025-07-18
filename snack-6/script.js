fc5ff9987fce129859e218de465eee815de2c65b
MelaniaPicone <melania_picone@yahoo.it>
2025-07-18 09:20:28 +0200

    [add] 6 snack

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,17 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const wHard = zucchine.filter ((weight) =>
+{
+
+if(zucchine.weight >= 15){
+  return true;
+}
+
+return zucchine.weight >= 15
+
+}
+)
+
+console.log(wHard);
